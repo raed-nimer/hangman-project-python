6167a87768f2110beea995bbbb563f03ae2afd86
Raed Nimer <raed.nimer7@gmail.com>
2024-09-08 17:10:36 +0000

    Random words selection

--- project.py
+++ project.py
@@ -2,3 +2,7 @@ import random
 # Adding words that are going to be checked
 
 words_list = ["ENGLAND", "GERMANY", "SCOTLAND", "CANADA", "MALTA", "SWEDEN"]
+
+# Pick a random word
+chosen_word = random.choice(words_list) 
+
