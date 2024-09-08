247dc64948fa83b2a0c7950a08c6cbbd54aef315
Raed Nimer <raed.nimer7@gmail.com>
2024-09-08 17:18:38 +0000

    Add user failed logic

--- project.py
+++ project.py
@@ -18,3 +18,27 @@ display = [] # []
 # Show letter block 
 for i in range(len(chosen_word)): #0 - 5
     display += "_" 
+
+
+print(display)
+
+# Accepting input from user
+while is_game_over == False:
+    user_input = input("Guess a letter:") #user_input = 'S'
+
+    for position in range(len(chosen_word)): # 0 - 5
+        letter = chosen_word[position] 
+        if user_input == letter:
+            display[position] = letter
+
+    print(display)
+  
+    # If letter does not exist in the chosen word 
+    if user_input not in chosen_word:
+        #decrease a life 
+        lives -= 1
+        if lives == 0: 
+            # User lost 
+            is_game_over = True 
+            print("You died!")     
+    
