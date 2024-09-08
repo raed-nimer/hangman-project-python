3ad1cb51a4731aaf9582df273f7143f6818f2bb6
Raed Nimer <raed.nimer7@gmail.com>
2024-09-08 17:14:51 +0000

    Display empty character spaces

--- project.py
+++ project.py
@@ -5,4 +5,16 @@ words_list = ["ENGLAND", "GERMANY", "SCOTLAND", "CANADA", "MALTA", "SWEDEN"]
 
 # Pick a random word
 chosen_word = random.choice(words_list) 
+print(chosen_word)
 
+
+# lives of the character
+lives = 5
+
+is_game_over = False
+
+#chosen = SWEDEN
+display = [] # []
+# Show letter block 
+for i in range(len(chosen_word)): #0 - 5
+    display += "_" 
