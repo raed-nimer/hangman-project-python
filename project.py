8dd49c7c7db379d5e4cbef72f7b891c06c944e98
Raed Nimer <raed.nimer7@gmail.com>
2024-09-19 13:10:09 +0000

    Add improvements

--- project.py
+++ project.py
@@ -58,7 +58,7 @@ YL = fg("light_yellow")
 R = attr("reset")
 
 INITIAL_BANNER = SB + pyfiglet.figlet_format(
-    "WELCOME TO HANGMAN!", font="rectangles", justify="center"
+    "Python Hangman Game!", font="rectangles", justify="center"
     ) + R 
 SUCCESS_MESSAGE = GR + pyfiglet.figlet_format(
     "YOU WON!", font="rectangles", justify="center"
